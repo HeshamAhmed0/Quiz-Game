11536457fdad731230461dd5fca42e56aaf702ab
Hesham Mathana <heshammathana0@gmail.com>
2025-02-16 16:19:44 +0200

    Handle Error

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,15 +6,14 @@
         {
             Player player = new Player();
             Console.Write("Enter Your Name : ");
-            player.SetName(Console.ReadLine());
+            player.SetName(Console.ReadLine()?? "No Name");
             Console.Write("Enter Your Id : ");
-            player.SetId(int.Parse(Console.ReadLine()));
+            player.SetId(int.Parse(Console.ReadLine()?? "0"));
             Console.Clear();
             Questions questions = new Questions();
             questions.Start();
             player.SetScore( questions.GetScore());
-
-
+            Console.WriteLine(player);
         }
     }
 }
